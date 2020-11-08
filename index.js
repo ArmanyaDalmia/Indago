84b4abfb21fa2e0df210b94e70796efff4069fed
Stephenwang3801 <stephenwang3801@gmail.com>
2020-11-08 09:06:51 -0500

    Added scheduling functionality to the bot.
    Added commands so users may interact with the bot, which is intended to help the user schedule their day and workload.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+const Schedule = require('./scheduleClass.js');
 const Discord = require("discord.js"); 
 const config = require('./config.json');
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest; 
 const client = new Discord.Client();
 const prefix = "!"; 
+const scheduleArr = [];
 
 function invoke(action, version, params={}) {
     return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
-        console.log('HEREERERE');
         xhr.addEventListener('error', () => reject('failed to issue request'));
         xhr.addEventListener('load', () => {
             try {
@@ -35,52 +36,104 @@ function invoke(action, version, params={}) {
     });
 }
 
-
-
 client.on("message", async function(message) {
     if(message.author.bot||!message.content.startsWith(prefix)) {
         return;
     }
 
     const commandBody = message.content.slice(prefix.length); 
-    const args = commandBody.split(' '); 
-    const command = args.shift().toLowerCase();
 
-    if (command == "ping"){ 
-        const timeTaken = Date.now() - message.createdTimestamp; 
-        message.reply(`Daniel's a bitch! This message had a latency of ${timeTaken} ms.`); 
-    }
-
-    if (command == "mydecks"){
-        const result = await invoke('deckNames', 6);
-        message.reply(`These are your current decks: ${result}`);
-    }
-
-    if (command == "listcards"){ 
-        const result = await invoke('findCards', 6, {"query": `deck:${args}`});
-        message.reply(`These are the cards in ${args}: ${result}`);
-    }
-
-
-    //Schedule Section
+    const tempCommand = commandBody.split(" ", 1);
+    const args = commandBody.replace(tempCommand + " ", "");
+    const command = tempCommand[0].toLowerCase();
     
-    if (command === "CreateSchedule") {//Allows user to create a schedule
+    //Daily Planner/Scheduling function section
+    if (command === "createschedule") {//Allows user to create a schedule
+        let name = args;
+        if(existChecker(name)){
+            message.reply("The schedule you named already exist, you may add to it instead or choose to make a new one!");
+        }else{
+            var schedule = new Schedule (name);
+            scheduleArr.push(schedule);
+            message.reply(`Your new schedule ${name} has been successfully created, tasks may now be added`);
+        }
+
+
+    }else if (command === "fillschedule"){//Allows user to fill schedule with tasks
+        let nameOfList = args.split (" ", 1);
+        let chores = args.replace (nameOfList + " ", "");
+        let tasks = chores.split (" ");
     
-    
-    }else if (command === "RetrieveSchedule") {//Allows user to check a schedule given a name
+        if(existChecker(nameOfList)){ //checks input validity
+            
+            for(i = 0; i < scheduleArr.length; i++){ //cycles schedule array
+                if(scheduleArr[i].name == nameOfList){
+                    for(j = 0; j < tasks.length; j++){
+                        scheduleArr[i][j] = tasks[j]; 
+                    }
+                    message.channel.send("Your new tasks have been uploaded, I'll remember em for ya!");
+                }
+            }
+        }else{
+            message.reply(`Sorry the schedule you requested does not exist, display if needed to find the correct list name`);
+        }
+
+
+    }else if (command === "retrieveschedule") {//Allows user to check a schedule given a name
+        let nameOfList = args.split (" ", 1);
         
-        
-    }else if (command === "DisplayMySchedules") {//Allows user to check a schedule given a name
-        
+        if (existChecker(nameOfList)){
+            message.reply("your schedule is as follows:");
+            for(i = 0; i < scheduleArr.length; i++){
+                if(scheduleArr[i].name == nameOfList){ 
+                    let size = Object.keys(scheduleArr[i]).length; 
+                    for(j = 0; j < size; j++){
+                        message.channel.send(scheduleArr[i][j]);
+                    }
+                }
+            }    
+        }else{
+            message.reply(`Sorry the schedule you requested does not exist, display if needed to find the correct list name`);
+        }
+    }else if (command === "displaymyschedules") {//Allows user to check a schedule given a name
+        //verifies if there are any schedules
+        if(scheduleArr.length == 0){
+            message.reply("It appears as though you don't have any schedules, make some and we'll keep track of them for you!");
+        }else{
+            message.reply("These are all your schedules");
+            for (i = 0; i < scheduleArr.length; i++){
+                message.channel.send()
+                message.channel.send((i + 1) + ". \t" + scheduleArr[i].name);
+            }
+        }
 
-    }else if (command === "DeleteSchedule") {//Allows user to delete a schedule given a name
+    }else if (command === "deleteschedule") {//Allows user to delete a schedule given a name
+        let deleteName = args;
+        let counter = 0;
+        //checks for the existence of said schedule and deletes the element at the index
+        for(i = 0; i < scheduleArr.length; i++){
+            if(scheduleArr[i].name == deleteName){
+                scheduleArr.splice(i, 1);
+                counter++;
+            }
+        }
+        if(counter == 0){
+            message.reply("Sorry the schedule you requested does not exist, display if needed to find the correct list name");
+        }else{
+            message.reply(deleteName + " has been deleted from your schedules!");
+        }
 
-
-    }else{//If the command does not exist, an error message is displayed and a guide to reinform the user will be opened
-        message.reply('Sorry, I do not recognize that command...');
     }
-    
-
 }); 
+
+function existChecker(listName){
+    let exist = false;
+    for(i = 0; i < scheduleArr.length; i++){
+        if(scheduleArr[i].name == listName){
+            exist = true;
+        }
+    }
+    return exist;
+}
 
 client.login(config.BOT_TOKEN); 
