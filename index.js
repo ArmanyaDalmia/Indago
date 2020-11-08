b5d74fb156c6cce73ddf4383bc53d575e5d7b838
ArmanAth <arman.ath12@gmail.com>
2020-11-08 09:55:30 -0500

    Commiting Daniel's changes to main

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const request = require('request-promise');
 const tough = require('tough-cookie');
 
+const ytdl = require("ytdl-core");
+const {Translate} = require('@google-cloud/translate').v2;
+const translate = new Translate();
+
 const client = new Discord.Client();
 const prefix = "!"; 
 const scheduleArr = [];
@@ -42,6 +46,7 @@ const helpText = ['!mydecks - Get a list of all the decks you currently have on 
 const querystring = require('querystring');
 const fetch = require("node-fetch");
 
+var servers = {};
 
 function invoke(action, version, params={}) {
     return new Promise((resolve, reject) => {
@@ -70,6 +75,33 @@ function invoke(action, version, params={}) {
 
         xhr.open('POST', 'http://127.0.0.1:8765');
         xhr.send(JSON.stringify({action, version, params}));
+    });
+}
+
+async function translateText(message, text, tar) {
+    const toText = text;
+    const target = tar;
+
+    let [translations] = await translate.translate(toText, target);
+    translations = Array.isArray(translations) ? translations : [translations];
+    translations.forEach((translation, i) => {
+      message.channel.send(`(${target}) ${translation}`);
+    });
+}
+
+async function listLanguages(message) {
+    let [languages] = await translate.getLanguages();
+    languages.forEach((language, i) => {
+        message.channel.send(language.name + " (" + language.code + ")");
+    });
+}
+
+function makeChannels(message) {
+    roomStarter = new String(message.author.username)
+    channelName = '-study';
+    message.guild.channels.create(roomStarter.concat(channelName), { reason: 'Needed a cool new channel' });
+    message.guild.channels.create(roomStarter.concat(channelName), {
+        type: 'voice',
     });
 }
 
@@ -202,6 +234,8 @@ client.on("message", async function(message) {
     //Only read the message as a command if it has the correct prefix and if it is not send by a bot
     if(message.author.bot) return; 
     if(!message.content.startsWith(prefix)) return; 
+
+    const d_args = message.content.substring(PREFIX.length).split(" ");
 
     //Seperating the command from the arguments 
     const commandBody = message.content.slice(prefix.length); 
@@ -478,6 +512,78 @@ client.on("message", async function(message) {
     else if (!otherCommands.includes(command)){ 
         message.reply(`You entered an invalid command! Please use !help to get a list of commands.`)
     }
+
+    switch (d_args[0]) {
+        case 'play':
+            function play(connection, message) {
+                var server = servers[message.guild.id];
+                if (!server.queue[1]) {
+                    server.dispatcher = connection.play(ytdl(server.queue[0], { filter: "audioonly" }));
+                    server.dispatcher.on("finish", function() {
+                        server.queue.shift();
+                        if (server.queue[0]) {
+                            play(connection, message);
+                        } else {
+                            server.queue.push(d_args[1]);
+                        }
+                    });
+                }
+            }
+
+            if (!d_args[1]) {
+                message.channel.send("Provide a link");
+                return;
+            }
+
+            if (!message.member.voice.channel) {
+                message.channel.send("You must be in a channel to play the bot");
+                return;
+            }
+
+            if (!servers[message.guild.id]) servers[message.guild.id] = {
+                queue: []
+            }
+
+            var server = servers[message.guild.id];
+            server.queue.push(d_args[1]);
+
+            if (!message.guild.voiceConnection) message.member.voice.channel.join().then(function(connection) {
+                play(connection, message);
+                })
+            break;
+
+        case 'skip':
+            var server = servers[message.guild.id];
+            if (server.dispatcher) server.dispatcher.end();
+            message.channel.send("Skipping song");
+            break;
+
+        case 'stop':
+            var server = servers[message.guild.id];
+            if (message.guild.voice.connection) {
+                for (var i = server.queue.length - 1; i >= 0; i--) {
+                    server.queue.splice(i, 2);
+                }
+
+                server.dispatcher.end();
+                message.channel.send("Stopping song");
+            }
+
+        case 'study':
+            makeChannels(message);
+            message.channel.send("Join your study channels");
+
+        case 'languages':
+            listLanguages(message);
+
+        case 'translate':
+            text = message.content.substring(message.content.indexOf(" ") + 1);
+            transArg = d_args[1];
+            text = text.substring(text.indexOf(" ") + 1);
+            translateText(message, text, transArg);
+    }
+    if (message.guild.connection) message.guild.voice.connection.disconnect();
+    
 });
 
 //Create a discord client for the bot using private token
