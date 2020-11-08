1a2f7c7ac0f2d7ebc3026cd7192a73d52e48db2b
David <46908974+davidtran001@users.noreply.github.com>
2020-11-07 19:09:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,63 @@ const config = require('./config.json');
 const client = new Discord.Client();
 const prefix = "!"; 
 
+<<<<<<< Updated upstream
+=======
+function invoke(action, version, params={}) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        console.log('HEREERERE');
+        xhr.addEventListener('error', () => reject('failed to issue request'));
+        xhr.addEventListener('load', () => {
+            try {
+                const response = JSON.parse(xhr.responseText);
+                if (Object.getOwnPropertyNames(response).length != 2) {
+                    throw 'response has an unexpected number of fields';
+                }
+                if (!response.hasOwnProperty('error')) {
+                    throw 'response is missing required error field';
+                }
+                }
+                if (response.error) {
+                    throw response.error;
+                }
+                resolve(response.result);
+            } catch (e) {
+                reject(e);
+            }
+        });
+
+        xhr.open('POST', 'http://127.0.0.1:8765');
+        xhr.send(JSON.stringify({action, version, params}));
+    });
+}
+
+client.on("message", async function(message) {
+    if(message.author.bot) return; 
+    if(!message.content.startsWith(prefix)) return; 
+    const commandBody = message.content.slice(prefix.length); 
+    const args = commandBody.split(' '); 
+    const command = args.shift().toLowerCase();
+
+    if (command == "ping"){ 
+        const timeTaken = Date.now() - message.createdTimestamp; 
+        message.reply(`Daniel's a bitch! This message had a latency of ${timeTaken} ms.`); 
+    }
+
+    if (command == "mydecks"){
+        const result = await invoke('deckNames', 6);
+        message.reply(`These are your current decks: ${result}`);
+    }
+
+    if (command == "listcards"){ 
+        const result = await invoke('findCards', 6, {"query": `deck:${args}`});
+        message.reply(`These are the cards in ${args}: ${result}`);
+    }
+	if (command == "createdeck"){
+		const result = await invoke('createdeck', 6, {deck:
+	    console.log(args);
+        message.reply(Created deck: ${result});
+}); 
+
+>>>>>>> Stashed changes
 client.login(config.BOT_TOKEN); 
