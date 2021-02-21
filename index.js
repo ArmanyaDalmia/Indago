fc5405bfaeaa6a50a4d4a1bad900c92b2fd4dc1f
Armanya Dalmia <armanya.dalmia@gmail.com>
2021-02-21 00:09:46 -0500

    Setup web app for discord bot

--- index.js
+++ index.js
@@ -665,3 +665,32 @@ client.on("message", async function (message) {
 //Create a discord client for the bot using private token
 //client.login(config.BOT_TOKEN);
 client.login(process.env.BOT_TOKEN);
+
+const express = require("express");
+const app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const port = process.env.PORT || 5000;
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
+
+// make express look in the `public` directory for assets (css/js/img)
+app.use(express.static(__dirname + "/public"));
+
+// set the home page route
+app.get("/", (request, response) => {
+  // ejs render automatically looks in the views folder
+  response.render("index");
+});
+
+app.listen(port, () => {
+  // will echo 'Our app is running on http://localhost:5000 when run locally'
+  console.log("Our app is running on http://localhost:" + port);
+});
+
+// pings server every 15 minutes to prevent dynos from sleeping
+setInterval(() => {
+  http.get("http://indago-discord.herokuapp.com");
+}, 900000);
